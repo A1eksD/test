7a39362e494ddfbb64a9e4fa11148d6adce8b930
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-07-10 13:00:08 +0200

    added gitignore file

---
+++ up.bat
@@ -0,0 +1,10 @@
+//für windows
+//beim ausführen in CMD folgendes eintippen: up und die commit-beschreibung
+//bei bedarf, andere Befehle nachtragen
+
+git pull
+git add .
+git commit -m "%*"
+git push
+
+
---
+++ up.sh
@@ -0,0 +1,8 @@
+#für linux / mac
+#beim ausführen in CMD folgendes eintippen: up und die commit-beschreibung
+#bei bedarf, andere Befehle nachtragen
+
+git pull
+git add .
+git commit -m "$*"
+git push
